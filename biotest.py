c0b88998460b0a04b3ecd4090d5149d7c8f354eb
singularityu <rich.bodo@singularityu.org>
2015-01-31 10:13:27 -0800

    split up the dtrust example into two functions and convert to btctest

--- biotest.py
+++ biotest.py
@@ -51,10 +51,9 @@ def test_block_io(bio_api_key, bio_spin):
     block_io = BlockIo(bio_api_key, bio_spin, 2)
     print_flat(block_io.get_my_addresses())
 #
-# Straight up port of the block.io dtrust.py example
+# Straight up copy of the block.io dtrust.py example
 #
 def block_io_dtrust_example(bio_api_key, bio_spin):
-
     version = 2 # API version
 
     # use a testnet api key here, say, dogecoin
@@ -161,39 +160,106 @@ def block_io_dtrust_example(bio_api_key, bio_spin):
 #
 # Create the award transaction
 #
-# def create_award_transaction(bio_api_key, bio_spin):
-#      print "tbd create_award_xact"
-#      version = 2 # API version
-
-#      # Get the BlockIo API object
-#      block_io = BlockIo(bio_api_key, bio_spin, version)
-#      getcontext().prec = 8 # coins are 8 decimal places at most
-
-#      # Create a new address with a random label
-#      address_label = 'dtrust'+str(int(random.random()*10000))
-
-
-
+# Create a transaction awarding coin to a multisig script
+# basically the first half of the dtrust example - using btctest
+# Input:
+# Return:
 #
-# Test the award transaction
-#
-# def test_create_award_transaction():
-#     print "tbd create_award_xact"
-#
+def create_award_transaction(bio_api_key, bio_spin):
+    version = 2 # API version
+
+    # using bitcoin testnet
+    block_io = BlockIo(bio_api_key, bio_spin, version)
+    getcontext().prec = 8 # coins are 8 decimal places at most
+
+    # create a new address with a random label
+    address_label = 'dtrust'+str(int(random.random()*10000))
+
+    print "The dtrust address label is: " + address_label
+
+    # create the key objects for each private key
+    keys = [ BlockIo.Key.from_passphrase('alpha1alpha2alpha3alpha4'), BlockIo.Key.from_passphrase('alpha2alpha3alpha4alpha1'), BlockIo.Key.from_passphrase('alpha3alpha4alpha1alpha2'), BlockIo.Key.from_passphrase('alpha4alpha1alpha2alpha3') ]
+
+    pubkeys = []
+
+    for key in keys:
+        pubkeys.insert(len(pubkeys), key.pubkey_hex())
+        six.print_(key.pubkey_hex())
+
+    # create a dTrust address that requires 4 out of 5 keys (4 of ours, 1 at Block.io).
+    # Block.io automatically adds +1 to specified required signatures because of its own key
+
+    print "* Creating a new 4 of 5 MultiSig address for BTCTEST"
+    six.print_(','.join(str(x) for x in pubkeys))
+    response = block_io.get_new_dtrust_address(label=address_label,public_keys=','.join(str(x) for x in pubkeys),required_signatures=3)
+
+    # if you want this to be a green address (instant coin usage), add make_green=1 to the above call's parameters
+    # if choosing a green address, you will not receive a redeem_script in the response
+    # this is because Block.io must guarantee against double spends for green addresses
+
+    # what's our new address?
+    new_dtrust_address = response['data']['address']
+    six.print_(">> New dTrust Address on Network=", response['data']['network'], "is", new_dtrust_address)
+
+    # save this redeem script so you can use this address without depending on Block.io
+    six.print_(">> Redeem Script:", response['data']['redeem_script'])
+
+    # let's deposit some coins into this dTrust address of ours
+    six.print_("* Sending .008 BTCTEST to", new_dtrust_address)
+    response = block_io.withdraw_from_labels(from_labels='default', to_addresses=new_dtrust_address, amounts='.008')
+    six.print_(">> Transaction ID:", response['data']['txid']) # you can check this on SoChain or any other blockchain explorer immediately
+
 
 #
 # Create the bounty transaction
 #
-# def create_bounty_transaction():
-#     print "tbd create_bounty_xact"
+# Create a transaction awarding coin to the editor
+# Basically the second half of the dtrust example
 #
+# Input: editor_rcv_addy
+# Returns:
+#
+def create_bounty_transaction(editor_rcv_address):
+    # create the withdrawal request
+    six.print_("* Creating withdrawal request")
 
-#
-# Test the bounty transaction
-#
-# def create_bounty_transaction():
-#     print "tbd create_bounty_xact"
-#
+    response = block_io.withdraw_from_dtrust_addresses(from_addresses=new_dtrust_address,to_addresses=default_address,amounts=("%0.8f" % amount_to_send))
+
+    # the response contains data to sign and all the public_keys that need to sign it
+    # you can distribute this response to all of your machines the contain your private keys
+    # and have them inform block.io after signing the data
+    # from anywhere, you can then finalize the transaction
+
+    # below, we take this response, extract the data to sign, sign it and inform Block.io of the signatures right after we make them
+    # for one key at a time
+    six.print_(">> Withdrawal Reference ID:", response['data']['reference_id'])
+
+    # sign the withdrawal request, one signature at a time
+
+    for key in keys:
+
+        for input in response['data']['inputs']:
+
+            data_to_sign = input['data_to_sign']
+
+            # find the object to put our signature in
+            for signer in input['signers']:
+
+                if signer['signer_public_key'] == key.pubkey_hex():
+                    # found it, let's add the signature to this object
+                    signer['signed_data'] = key.sign_hex(data_to_sign)
+
+                    six.print_("* Data Signed By:", key.pubkey_hex())
+
+        # let's have Block.io record this signature we just created
+        block_io.sign_transaction(signature_data=json.dumps(response['data']))
+        six.print_(">> Signatures relayed to Block.io for Public Key=", key.pubkey_hex())
+
+    # finalize the transaction now that's it been signed by all our keys
+    six.print_("* Finalizing transaction")
+    response = block_io.finalize_transaction(reference_id=response['data']['reference_id'])
+    six.print_(">> Transaction ID:", response['data']['txid'])
+    six.print_(">> Network Fee Incurred:", response['data']['network_fee'], response['data']['network'])
 
 #
 # Grab the secrets out of an ini file so we don't have to check them into git!
@@ -231,9 +297,9 @@ def main():
 
     #test_block_io(mb_secrets.bio_api_key,mb_secrets.bio_spin)
 
-    block_io_dtrust_example(mb_secrets.bio_doge_api_key, mb_secrets.bio_spin)
+    #block_io_dtrust_example(mb_secrets.bio_doge_api_key, mb_secrets.bio_spin)
 
-
+    create_award_transaction(mb_secrets.bio_api_key, mb_secrets.bio_spin)
 
 if __name__ == '__main__':
     main()
