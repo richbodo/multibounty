aa126d9c9c5cef534e9b56df4878570dcb1f632f
Zvi Boshernitzan <zvi@kiva.org>
2015-02-28 21:51:54 -0800

    running multibounty under meteor

--- apis.js
+++ apis.js
@@ -1,5 +1,5 @@
 // module apis
-var SECRETS = require('/home/rsb/.multibounty/secrets.js').SECRETS
+var SECRETS = require('/Users/zvi/.multibounty/secrets.js').SECRETS
 var BlockIo = require('block_io');
 var version = 2; // API version
 exports.platform = new BlockIo(SECRETS["platform"]["bitcoin_testnet_api_key"], 
--- bde_site/multibounty.js
+++ bde_site/multibounty.js
@@ -1,7 +1,5 @@
 var Bounties = new Mongo.Collection("bounties");
 
-
-
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -25,9 +23,26 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
+    // module apis
+    var SECRETS = Npm.require('/Users/zvi/.multibounty/secrets.js').SECRETS
+    var BlockIo = Npm.require('block_io');
+    var version = 2; // API version
+    var platform = new BlockIo(SECRETS["platform"]["bitcoin_testnet_api_key"], 
+				   SECRETS["platform"]["spin"], // 'YOUR SECRET PIN', 
+				   version);
+    var author =  new BlockIo(SECRETS["author"]["bitcoin_testnet_api_key"], 
+				  SECRETS["author"]["spin"], // 'YOUR SECRET PIN', 
+				  version);
+    
+
+
+    
+    Meteor.startup(function () {
+	// code to run on server at startup
+	console.log(platform);
+	console.log(author);
+    });
 }
 
 // methods that get executed on the client AND the server
