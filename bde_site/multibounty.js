d5efba8313a79402237f9b2b444f65f4c3b73ad3
Zvi Boshernitzan <zvi@kiva.org>
2015-02-28 22:46:20 -0800

    added blockio synchronization

--- bde_site/multibounty.js
+++ bde_site/multibounty.js
@@ -3,23 +3,49 @@ var Bounties = new Mongo.Collection("bounties");
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
-
-  
- 
-
     
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+	counter: function () {
+	    return Session.get('counter');
+	},
+	platformBalance : function() {
+	    return Session.get('platformBalance');
+	},
+	authorBalance : function() {
+	    return Session.get('authorBalance');
+	}
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    var updateCounter = function() {
+	Meteor.call('get',function(error,result){
+	    Session.set('counter', result);
+	});
+    };
+    
+    Template.hello.events({
+	'click .incr' : function() {
+	    Meteor.call('incr',updateCounter);
+	},
+	'click .decr' : function() {
+	    Meteor.call('decr', updateCounter);
+	    
+	},
+	'click .show': function () {
+	    // increment the counter when button is click
+	    updateCounter();
+	},
+	'click .update' : function() {
+	    Meteor.call('updateBalances');
+	    Meteor.call("getPlatformBalance",function(error,value){
+		Session.set('platformBalance',value);
+	    });
+	    Meteor.call("getAuthorBalance",function(error,value){
+		Session.set('authorBalance',value);
+	    });
+	}
+	
+    });
+
 }
 
 if (Meteor.isServer) {
@@ -29,19 +55,49 @@ if (Meteor.isServer) {
     var BlockIo = Npm.require('block_io');
     var version = 2; // API version
     var platform = new BlockIo(SECRETS["platform"]["bitcoin_testnet_api_key"], 
-				   SECRETS["platform"]["spin"], // 'YOUR SECRET PIN', 
-				   version);
+			       SECRETS["platform"]["spin"], // 'YOUR SECRET PIN', 
+			       version);
     var author =  new BlockIo(SECRETS["author"]["bitcoin_testnet_api_key"], 
-				  SECRETS["author"]["spin"], // 'YOUR SECRET PIN', 
-				  version);
+			      SECRETS["author"]["spin"], // 'YOUR SECRET PIN', 
+			      version);
+
+    var total = 0;
+    var balances = {
+	'platform': 0,
+	'author': 0
+    };
+    var showTotal = function() { console.log(['total',total]); }
+    Meteor.methods({
+	incr : function() { total += 1; showTotal() },
+	decr : function() { total -= 1; showTotal() },
+	get : function() { showTotal(); return total; },
+	//- ----------
+	getPlatformBalance : function() {
+	    return balances['platform'];
+	},
+	getAuthorBalance : function() {
+	    return balances['author'];
+	},
+	updateBalances : function() {
+	    platform.get_balance(function(error,result){
+		balances['platform'] = result.data.available_balance;
+	    });
+	    author.get_balance(function(error,result){
+		balances['author'] = result.data.available_balance;
+	    });
+	},
+	transferSomeBitcoin : function() {
+
+	}
+	
+    });
     
-
-
     
     Meteor.startup(function () {
 	// code to run on server at startup
 	console.log(platform);
 	console.log(author);
+	Meteor.call('updateBalances');
     });
 }
 
