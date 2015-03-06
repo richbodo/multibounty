92c0caf9b87f425f29cc68b2735f10751bf6aba0
singularityu <rich.bodo@singularityu.org>
2015-03-05 21:03:46 -0800

    attempting to add some collection data

--- bde_site/multibounty.js
+++ bde_site/multibounty.js
@@ -2,55 +2,61 @@ var Bounties = new Mongo.Collection("bounties");
 
 if (Meteor.isClient) {
 
-  foo="hello";
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-    
+    Meteor.subscribe("bounties");
+
+    // counter starts at 0
+    Session.setDefault('counter', 0);
+
     Template.hello.helpers({
-	counter: function () {
-	    return Session.get('counter');
-	},
-	platformBalance : function() {
-	    return Session.get('platformBalance');
-	},
-	authorBalance : function() {
-	    return Session.get('authorBalance');
-	}
+        counter: function () {
+            return Session.get('counter');
+        },
+        platformBalance: function () {
+            return Session.get('platformBalance');
+        },
+        authorBalance: function () {
+            return Session.get('authorBalance');
+        }
     });
 
-    var updateCounter = function() {
-	Meteor.call('get',function(error,result){
-	    Session.set('counter', result);
-	});
+    var updateCounter = function () {
+        Meteor.call('get', function (error, result) {
+            Session.set('counter', result);
+        });
     };
-    
+
     Template.hello.events({
-	'click .incr' : function() {
-	    Meteor.call('incr',updateCounter);
-	},
-	'click .decr' : function() {
-	    Meteor.call('decr', updateCounter);
-	    
-	},
-	'click .show': function () {
-	    // increment the counter when button is click
-	    updateCounter();
-	},
-	'click .update' : function() {
-	    Meteor.call('updateBalances');
-	    Meteor.call("getPlatformBalance",function(error,value){
-		Session.set('platformBalance',value);
-	    });
-	    Meteor.call("getAuthorBalance",function(error,value){
-		Session.set('authorBalance',value);
-	    });
-	}
+        'click .incr': function () {
+            Meteor.call('incr', updateCounter);
+        },
+        'click .decr': function () {
+            Meteor.call('decr', updateCounter);
+
+        },
+        'click .show': function () {
+            // increment the counter when button is click
+            updateCounter();
+        },
+        'click .update': function () {
+            Meteor.call('updateBalances');
+            Meteor.call("getPlatformBalance", function (error, value) {
+                Session.set('platformBalance', value);
+            });
+            Meteor.call("getAuthorBalance", function (error, value) {
+                Session.set('authorBalance', value);
+            });
+        }
 
     });
 
     Template.create_bounty.events({
         'click .create_bounty': function () {
-            alert($(".creative_work").val());
+            //alert($(".creative_work").val());
+            var creative_work = $(".creative_work").val();
+            var bounty_task = $(".bounty_task").val();
+            var bounty_amount = $(".bounty_amount").val();
+            // add bounty to authors bounty list
+            Meteor.call("addBounty", creative_work, bounty_task, bounty_amount);
         }
     });
 }
@@ -109,4 +115,31 @@ if (Meteor.isServer) {
 }
 
 // methods that get executed on the client AND the server
+Meteor.methods ({
+    addBounty: function (creative_work, bounty_task, bounty_amount) {
+        console.log("adding a Bounty to the db in meteor method");
+        // Make sure the user is logged in before inserting a Bounty
+        if (!Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
 
+        Bounties.insert({
+            creative_work: creative_work,
+            bounty_task: bounty_task,
+            bounty_amount: bounty_amount,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    },
+
+    deleteBounty: function (BountyId) {
+        var Bounty = Bounties.findOne(BountyId);
+        if (Bounty.owner !== Meteor.userId()) {
+            // If the Bounty is private, make sure only the owner can delete it
+            throw new Meteor.Error("not-authorized");
+        }
+
+        Bounties.remove(BountyId);
+    }
+})
