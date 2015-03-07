ed6fca3b46c98597ced40f5b62eb663ba3e4c791
singularityu <rich.bodo@singularityu.org>
2015-03-06 23:54:19 -0800

    add back user stuff

--- bounty_meteor/collections/bounties.js
+++ bounty_meteor/collections/bounties.js
@@ -4,25 +4,25 @@ Meteor.methods({
     addBounty: function (creative_work, bounty_task, bounty_amount) {
         console.log("adding a Bounty to the db in meteor method");
         // Make sure the user is logged in before inserting a Bounty
-        //if (!Meteor.userId()) {
-        //    throw new Meteor.Error("not-authorized");
-        //}
+        if (!Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
 
         Bounties.insert({
             creative_work: creative_work,
             bounty_task: bounty_task,
             bounty_amount: bounty_amount,
-            createdAt: new Date()
-            //owner: Meteor.userId(),
-            //username: Meteor.user().username
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
         });
     },
     deleteBounty: function (BountyId) {
         var Bounty = Bounties.findOne(BountyId);
-        //if (Bounty.owner !== Meteor.userId()) {
+        if (Bounty.owner !== Meteor.userId()) {
             // If the Bounty is private, make sure only the owner can delete it
-        //    throw new Meteor.Error("not-authorized");
-        //}
+            throw new Meteor.Error("not-authorized");
+        }
 
         Bounties.remove(BountyId);
     }
