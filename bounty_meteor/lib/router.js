7e84720e227f96dd24fde9f8d7d2a3de2260bccc
Zvi Boshernitzan <zvi@kiva.org>
2015-03-06 22:55:00 -0800

    got routing going

--- bounty_meteor/lib/router.js
+++ bounty_meteor/lib/router.js
@@ -1,3 +1,15 @@
-Router.route('/create',function() {
+Router.route('/bounties',function(){
+    this.render('bounties');
+});
+Router.route('/bounties/new',function() {
     this.render('create_bounty');
+});
+Router.route('/bounty/:id',function(){
+    this.render('bounty', {data: {bounty_id:this.params.id}});
+});
+Router.route('/bounty/:id/submissions/new',function(){
+    this.render('create_submission', {data: {bounty_id:this.params.id}});
+});
+Router.route('/submission/:id',function(){
+    this.render('submission', {data: {submission_id: this.params.id}});
 });
